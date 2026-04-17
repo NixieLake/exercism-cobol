bd05084de8ea4e3111f09d220fa63a8f505c16e4
NixieLake <coffeestar1118@gmail.com>
2026-04-17 12:58:49 -0400

    Add scoring logic for "Ones" category in Yacht implementation and update constants

--- cobol/yacht/src/yacht.cob
+++ cobol/yacht/src/yacht.cob
@@ -7,22 +7,27 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
       *
-      * Constant values.
+      * Constants
        01 CONSTANT-VALUES.
            05 WS-CONST-ALL-SAME-DIVISOR     PIC 9(5) VALUE 11111.
+           05 WS-CONST-NUM-DICE             PIC 9 VALUE 5.
+           05 WS-CONST-DIE-VALUES.
+               10 WS-CONST-DV-ONE           PIC 9 VALUE 1.
            05 WS-CONST-SET-SCORES.
                10 WS-CONST-SCORE-YACHT      PIC 99 VALUE 50.
       *
-      * Input/Output stores.
+      * Input/Output
        01 WS-RESULT                         PIC 99 VALUE 0.
        01 WS-CATEGORY                       PIC X(15).
        01 WS-DICE                           PIC 9(5).
       *
-      * Evaluation stores.
+      * Evaluation
        01 WS-DICE-LIST REDEFINES WS-DICE.
            05 WS-DIE                        PIC 9 OCCURS 5 TIMES.
+       01 WS-DICE-INDEX                     PIC 9 VALUE 0.
+       01 WS-DIE-VALUE-NEEDED               PIC 9 VALUE 0.
       *
-      * Calculation stores.
+      * Calculation
        01 WS-CALCULATIONS.
            05 WS-QUOTIENT                   PIC 9 VALUE 0.
            05 WS-REMAINDER                  PIC 9(5) VALUE 0.
@@ -31,8 +36,13 @@
       *
        YACHT.
       * Main program control.
+      *
+      *    Reset score.
+           MOVE ZERO                        TO WS-RESULT.
+      *    Calculate score based on category.
            EVALUATE WS-CATEGORY
                WHEN "yacht" PERFORM SCORE-YACHT
+               WHEN "ones" PERFORM SCORE-ONES
            END-EVALUATE.
            EXIT.
       *
@@ -50,3 +60,21 @@
                MOVE ZERO                    TO WS-RESULT
            END-IF.
       *
+       SCORE-ONES.
+      * Calculates score for Ones category.
+      *
+      *    Set the die value needed to 1.
+           MOVE WS-CONST-DV-ONE             TO WS-DIE-VALUE-NEEDED.
+      *    Iterate through each die.
+           PERFORM CHECK-DIE-VALUE VARYING WS-DICE-INDEX FROM 1 BY 1
+               UNTIL WS-DICE-INDEX > WS-CONST-NUM-DICE.
+      *
+       CHECK-DIE-VALUE.
+      * Checks if die value at index is the value needed. If so, adds
+      * the value to the score.
+      *
+      *    Check if die value is value needed.
+           IF WS-DIE(WS-DICE-INDEX) IS EQUAL TO WS-DIE-VALUE-NEEDED THEN
+      *        If so, add value to score.
+               ADD WS-DIE-VALUE-NEEDED      TO WS-RESULT
+           END-IF.
